2d37da90531a287cfa05904b23673dbf0fdce3d1
Shema <shemaserge23@gmail.com>
2024-04-29 11:11:08 +0200

    Fibonacci

--- Testing/index.js
+++ Testing/index.js
@@ -41,12 +41,34 @@
 
 // =====================Challenge====================================
 
-var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+// var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
 
-function whospaying(names){
-    var randomName = Math.random() * names.length
-    var paying = names[Math.floor(randomName)]
-    console.log(paying + " is paying the bill")
+// function whospaying(names){
+//     var randomName = Math.random() * names.length
+//     var paying = names[Math.floor(randomName)]
+//     console.log(paying + " is paying the bill")
+// }
+
+// whospaying(names)
+
+// ======================For loop================================
+
+function fibonacci(n) {
+    var output = []
+    if (n === 0){
+        output = [0]
+    }
+    else if (n === 1){
+        output = [0, 1]
+    }
+    else {
+        output = [0, 1]
+        for (var i = 2; i < n; i++){
+            output.push(output[i - 2] + output[i - 1])
+        }
+    }
+    return output
 }
 
-whospaying(names)
+var result = fibonacci(4)
+console.log(result)
