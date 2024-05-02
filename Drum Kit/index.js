2674e1e4e0a3fe74a87d3cdfe83a4370a8d93db7
Shema <shemaserge23@gmail.com>
2024-05-02 17:23:19 +0200

    Adding sound to javascript

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,1 +1,9 @@
-alert("Hello")
+var buttonsArray = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttonsArray.length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("./sounds/kick-bass.mp3")
+        audio.play()
+    })
+}
+
