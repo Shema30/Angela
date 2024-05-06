69bcd0a418dc38f216754cfde066825777a0cf95
Shema <shemaserge23@gmail.com>
2024-05-06 10:01:26 +0200

    call back functions

--- Testing/index.js
+++ Testing/index.js
@@ -1,74 +1,21 @@
-// var num = []
-// for (num.length = 0; num.length <= 100; num.length++){
-//     if (num.length % 3 == 0){
-//         num.push("Fizz")
-//     }
-//     else if (num.length % 5 == 0){
-//         num.push("Buzz")
-//     }
-//     else if (num.length % 3 == 0 && num.length % 5 == 0){
-//         num.push("FizzBuzz")
-//     }
+// function sum(callback,x,y) {
+//     let result = x + y
+//     callback(result)
 // }
 
-// console.log(num)
-
-// var output = []
-// var i = 1
-
-// function FizzBuzz() {
-
-//     if (i % 3 == 0) {
-//         output.push("fizz")
-//     }
-//     else if (i % 5 == 0) {
-//         output.push("buzz")
-//     }
-//     else if (i % 3 == 0 && i % 5 == 0) {
-//         output.push("fizzbuzz")
-//     }
-//     else {
-//         output.push(i)
-//     }
-//     i++
-//     console.log(output)
+// function displayConsole(result) {
+//     console.log(result)
 // }
 
-// FizzBuzz()
-// FizzBuzz()
-// FizzBuzz()
-// FizzBuzz()
+// sum(displayConsole, 1, 2)
 
-// =====================Challenge====================================
-
-// var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
-
-// function whospaying(names){
-//     var randomName = Math.random() * names.length
-//     var paying = names[Math.floor(randomName)]
-//     console.log(paying + " is paying the bill")
-// }
-
-// whospaying(names)
-
-// ======================For loop================================
-
-function fibonacci(n) {
-    var output = []
-    if (n === 0){
-        output = [0]
-    }
-    else if (n === 1){
-        output = [0, 1]
-    }
-    else {
-        output = [0, 1]
-        for (var i = 2; i < n; i++){
-            output.push(output[i - 2] + output[i - 1])
-        }
-    }
-    return output
+function hello (callback) {
+    console.log("hello")
+    callback()
 }
 
-var result = fibonacci(10)
-console.log(result)
+function goodbye () {
+    console.log("goodbye")
+}
+
+hello(goodbye);
