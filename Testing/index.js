0a157a4cf392db78b0c63a92b5d016ff4f9cc094
Shema <shemaserge23@gmail.com>
2024-05-07 10:31:34 +0200

    Adding JQuery Library

--- Testing/index.js
+++ Testing/index.js
@@ -1,21 +1,3 @@
-// function sum(callback,x,y) {
-//     let result = x + y
-//     callback(result)
-// }
-
-// function displayConsole(result) {
-//     console.log(result)
-// }
-
-// sum(displayConsole, 1, 2)
-
-function hello (call) {
-    console.log("hello")
-    call()
-}
-
-function goodbye () {
-    console.log("goodbye")
-}
-
-hello(goodbye);
+$(document).click(function(e) {
+    $("h1") 
+})
